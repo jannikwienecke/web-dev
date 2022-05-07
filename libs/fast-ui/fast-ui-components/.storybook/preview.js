51b6fa6ba2819f801069d20f3b1e5439ec767998
Jannik Wienecke <jannik.wienecke@sirum.de>
2022-05-07 09:11:07 +0100

    imp(App): add theme switcher

--- libs/fast-ui/fast-ui-components/.storybook/preview.js
+++ libs/fast-ui/fast-ui-components/.storybook/preview.js
@@ -1,10 +1,15 @@
 import './tailwind-imports.css';
-
-// export const parameters = {
-//   layout: 'centered',
-// };
+import { themes } from '@storybook/theming';
 
 export const parameters = {
+  themes: {
+    default: 'twitter',
+    list: [
+      { name: 'Dark Theme', class: 'theme-dark', color: '#00aced' },
+      { name: 'facebook', class: 'theme-fb', color: '#3b5998' },
+    ],
+  },
+
   backgrounds: {
     default: 'my-background',
     values: [
