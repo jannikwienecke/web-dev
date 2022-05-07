5be34fa20d4c921b222219924fef417d6f20280a
Jannik Wienecke <jannik.wienecke@sirum.de>
2022-05-07 10:01:01 +0100

    fix(Compoents): light/dark mode settings

--- libs/fast-ui/fast-ui-components/src/lib/SidebarAppDrawerItem/SidebarAppDrawerItem.tsx
+++ libs/fast-ui/fast-ui-components/src/lib/SidebarAppDrawerItem/SidebarAppDrawerItem.tsx
@@ -17,7 +17,7 @@ export function SidebarAppDrawerItem({
     group bg-skin-base relative
     `;
 
-  const tooltip = `tooltiptext hidden  absolute bg-skin-contrast p-1 px-3 text-skin-standard rounded-md top-3 left-16 text-white text-sm  group-hover:block whitespace-nowrap`;
+  const tooltip = `tooltiptext hidden absolute bg-skin-contrast p-1 px-3 text-skin-standard rounded-md top-3 left-16 text-white text-sm  group-hover:block whitespace-nowrap`;
 
   if (!appdrawerItem) return null;
   return (
@@ -27,7 +27,7 @@ export function SidebarAppDrawerItem({
       }`}
     >
       <div
-        className={`cursor-pointer bg-skin-base  text-skin-accent font-bold rounded-md flex flex-row items-center justify-center ${
+        className={`cursor-pointer bg-skin-base text-skin-accent font-bold rounded-md flex flex-row items-center justify-center ${
           isCollapsed || 'px-3  justify-start'
         } `}
       >
